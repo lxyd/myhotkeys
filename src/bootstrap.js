0c7c4d0dcb09c9b757d906e10477cb6138b613e5
Alexey Dubinin <DubininAlexey@gmail.com>
2015-05-26 15:35:12 +0300

    Introduce a-n hotkey to open new tab

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,6 +7,9 @@ const names = {
 }
 
 const handlers = [
+    { hotkey: parseHotkey('a-n'), handle: function newTab(window) {
+        window.gBrowser.selectedTab = window.gBrowser.addTab("about:blank")
+    }},
     { hotkey: parseHotkey('a-k'), handle: function nextTab(window) {
         window.gBrowser.tabContainer.advanceSelectedTab(1, true)
     }},
